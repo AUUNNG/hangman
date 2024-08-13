6ed2b4b9247c4e6be5d1e94b2c44b893bead7087
AUUNNG <aung.privates@gmail.com>
2024-08-13 22:55:34 +0700

    complete basic hangman ;-;

---
+++ words.py
@@ -0,0 +1,23 @@
+word_list = [
+    'apple',
+    'banana',
+    'cherry',
+    'date',
+    'elderberry',
+    'fig',
+    'grape',
+    'honeydew',
+    'kiwi',
+    'lemon',
+    'mango',
+    'nectarine',
+    'orange',
+    'papaya',
+    'quince',
+    'raspberry',
+    'strawberry',
+    'tangerine',
+    'ugli',
+    'vanilla',
+    'watermelon'
+]
